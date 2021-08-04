05877bae73b812f3606c38825ce42d0340cc4e4b
Trell <doboniy03@gmail.com>
2021-08-04 16:18:35 +0300

    field visualization

--- Assets/Scripts/LocalFieldGenerator.cs
+++ Assets/Scripts/LocalFieldGenerator.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class LocalFieldGenerator : MonoBehaviour
+namespace DarkLegion.Field.Local 
 {
-    // Start is called before the first frame update
-    void Start()
+    public class LocalFieldGenerator : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Tile _baseTile;
+        [SerializeField] private Tilemap _tilemap;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private Transform _startPoint;
+
+        [SerializeField] private Vector2Int _size;
+
+        private Vector3Int _initialCell;
+
+        private void Start()
+        {
+            _initialCell = _tilemap.WorldToCell(_startPoint.position);
+            Generate(_initialCell);
+        }
+
+        private void Generate(Vector3Int from)
+        {
+            for (int x = 0; x < _size.x; x++)
+            {
+                for(int  y = 0; y < _size.y; y++)
+                {
+                    _tilemap.SetTile(from + new Vector3Int(x, y, 0), _baseTile);
+                }
+            }
+        }
     }
 }
