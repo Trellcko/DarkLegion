005bcd35ba347b936f3eb1a2c62899627550c8db
Trell <doboniy03@gmail.com>
2021-08-06 12:33:42 +0300

    Pooling

--- Assets/Scripts/Field/Visualization/PathDrawer.cs
+++ Assets/Scripts/Field/Visualization/PathDrawer.cs
@@ -1,34 +1,32 @@
+using DarkLegion.Core.Pooling;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PathDrawer : MonoBehaviour
 {
-    [SerializeField] private GameObject _dot;
+    [SerializeField] private GameObjectPoolHandler _pool;
 
-    private List<GameObject> _dots = new List<GameObject>();
-    private List<SpriteRenderer> _dotsSpritesCashed = new List<SpriteRenderer>();
+    private readonly List<GameObject> _dots = new List<GameObject>();
 
     public void Draw(Vector3[] positions)
     {
         HideDots();
-        for(int i = 0; i < positions.Length; i++)
+        for(int  i = 0; i < positions.Length; i++)
         {
-            if(_dots.Count <= i )
-            {
-                _dots.Add(Instantiate(_dot, transform));
-                _dotsSpritesCashed.Add(_dots[i].GetComponent<SpriteRenderer>());
-            }
-            _dotsSpritesCashed[i].enabled = true;
-            _dots[i].transform.position = positions[i];
+            var dot = _pool.Get();
+            dot.transform.position = positions[i];
+            _dots.Add(dot);
         }
     }
 
     private void HideDots()
     {
-        foreach(var spriteRender in _dotsSpritesCashed)
+        foreach(var dot in _dots)
         {
-            spriteRender.enabled = false;
+            _pool.Add(dot);
         }
+        _dots.Clear();
     }
-
 }
--- Assets/Scripts/Field/Visualization/PathVisualization.cs
+++ Assets/Scripts/Field/Visualization/PathVisualization.cs
@@ -1,20 +1,22 @@
 using DarkLegion.Field.Pathfinding;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class PathVisualization : MonoBehaviour
+namespace DarkLegion.Field.Visuzalization
 {
-    [SerializeField] private Transform point;
-    [SerializeField] private Transform _endPoint;
-    
-    [SerializeField] private PathDrawer _drawer;
-
-    [SerializeField] private Pathfinder _pathfinder;
-
-    private void Update()
+    public class PathVisualization : MonoBehaviour
     {
-        _drawer.Draw(_pathfinder.FindPath(point.position, _endPoint.position).ToArray());
+        [SerializeField] private Transform point;
+        [SerializeField] private Transform _endPoint;
 
+        [SerializeField] private PathDrawer _drawer;
+
+        [SerializeField] private Pathfinder _pathfinder;
+
+        private void Update()
+        {
+            _drawer.Draw(_pathfinder.FindPath(point.position, _endPoint.position).ToArray());
+
+        }
     }
 }
