5692e1b49e91732d6052ce99bb557bae732830c0
Trell <doboniy03@gmail.com>
2021-08-10 21:52:09 +0300

    Refactoring

--- Assets/Scripts/PlayerCommander.cs
+++ Assets/Scripts/PlayerCommander.cs
@@ -33,26 +33,31 @@ public class PlayerCommander : MonoBehaviour
             if (path.Count <= _playersUnitSelecting.LastSelectedOrNull.UnitData.MaxStep && path.Count != 0)
             {
                 var commands = new Queue<ICommand>();
+             
                 var lastPoint = _playersUnitSelecting.LastSelectedOrNull.transform.position;
-
+                
                 foreach (var point in path)
-                {   
-                    if (Mathf.Abs(point.x - lastPoint.x) > 0.01f)
-                    {
-                        bool isLeft = point.x < lastPoint.x;
-                        commands.Enqueue(new FlipCommand(_playersUnitSelecting.LastSelectedOrNull.SpriteRender, isLeft));
-                    }
-                    else
-                    {
-                        bool isLeft = path[path.Count - 1].x < lastPoint.x;
-                        commands.Enqueue(new FlipCommand(_playersUnitSelecting.LastSelectedOrNull.SpriteRender, isLeft));
-                    }
+                {
+                    var targetFlipPoint = Mathf.Abs(point.x - lastPoint.x) > 0.01f ? point : path[path.Count - 1];
+
+                    commands.Enqueue(GetFlipCommand(lastPoint, targetFlipPoint));
+                    commands.Enqueue(GetMovementCommand(point));
 
                     lastPoint = point;
-                    commands.Enqueue(new MovementCommand(_playersUnitSelecting.LastSelectedOrNull.transform, point));
                 }
                 _playersUnitSelecting.LastSelectedOrNull.CommandHandler.Do(commands);
             }
         }
+    }
+
+    private MovementCommand GetMovementCommand(Vector3 point)
+    {
+        return new MovementCommand(_playersUnitSelecting.LastSelectedOrNull.transform, point);
+    }
+
+    private FlipCommand GetFlipCommand(Vector3 lastPoint, Vector3 point)
+    {
+        bool isLeft = point.x < lastPoint.x;
+        return new FlipCommand(_playersUnitSelecting.LastSelectedOrNull.SpriteRender, isLeft);
     }
 }
