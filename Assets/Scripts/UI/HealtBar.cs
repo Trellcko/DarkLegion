172fbb20493cd323e81bc9ab38b76660ce95b48a
Trellcko <32221886+Trellcko@users.noreply.github.com>
2021-08-27 00:10:10 +0300

    HealthBar

--- Assets/Scripts/UI/HealtBar.cs
+++ Assets/Scripts/UI/HealtBar.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
+using DG.Tweening;
+
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HealtBar : MonoBehaviour
+namespace DarkLegion.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HealtBar : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Image _fill;
+        [SerializeField] private TextMeshProUGUI _text;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private float _changeSpeed;
+
+        private float _maxValue = 1;
+        private float _currentValue = 1;
+
+        private const float MinValue = 0;
+
+        private void Start()
+        {
+            SetMax(100f);
+
+            SetValue(40f);
+        }
+
+        public void SetMax(float value)
+        {
+            if (value <= 0) return;
+
+            _currentValue *= value / _maxValue;
+            _maxValue = value;
+        }
+
+        public void SetValue(float value)
+        {
+            var clampedValue = Mathf.Clamp(value, MinValue, _maxValue);
+
+            float fillValue = clampedValue / _maxValue;
+
+            _fill.DOFillAmount(fillValue, _changeSpeed);
+
+            DOTween.To(() => _currentValue, (value) => _text.SetText($"{(int)value} / {_maxValue}"), clampedValue, _changeSpeed);
+        }
+
     }
 }
