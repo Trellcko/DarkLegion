cc46aadae514d399d8988493c7c58612b9ab22a9
Trellcko <32221886+Trellcko@users.noreply.github.com>
2021-08-27 13:16:44 +0300

    Attaching UI

---
+++ Assets/Scripts/Utils/AttachingUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DarkLegion.Utils
+{
+    public class AttachingUI : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _self;
+
+        [SerializeField] private Transform _target;
+        [SerializeField] private Vector3 _offset = new Vector3(0, 5);
+
+        private Vector3 _lastTargetPosition;
+
+        private void LateUpdate()
+        {
+            TryChangePosition();
+        }
+
+        public void SetTarget(Transform transform)
+        {
+            _target = transform;
+            _lastTargetPosition = transform.position;
+        }
+
+        private void TryChangePosition()
+        {
+            if (_target && Vector3.Distance(_target.position, _lastTargetPosition) > 0.01f)
+            {
+                _self.transform.position = _target.position + _offset;
+                _lastTargetPosition = _target.position;
+            }
+
+        }
+    }
+}
